1163d8225b7180177f38fff377df0bdd4fa096d8
PaulDelestre <71083544+PaulDelestre@users.noreply.github.com>
2020-11-17 16:59:56 +0100

    new 3

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+// src/Controller/DefaultController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{ 
+    /**
+     * @Route("/default/", name="default_index")
+     */
+    public function index(): Response
+    { 
+        return $this->render('program/index.html.twig', [
+            'website' => 'Bienvenues sur Wild Series',
+        ]);
+    }
+}
